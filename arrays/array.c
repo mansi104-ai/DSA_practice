2aee92cf5716f8a2a6fc62a0293b619eb18b8158
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-09-03 20:11:12 +0530

    compiler error to be solved

---
+++ arrays/array.c
@@ -0,0 +1,15 @@
+''' C program to illustrate the array declaration '''
+
+#include <stdio.h>
+
+int main()
+{
+
+    // declaring array of integers
+    int aar_int[5];
+
+    //declaring array of characters
+    char arr_char[5];
+
+    return 0;
+}
